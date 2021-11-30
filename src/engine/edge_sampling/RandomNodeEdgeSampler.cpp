015853639b7b365d57d47529a76c174404cde435
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-11-30 21:22:18 +0800

    refactor:　无向图保存完整的对称矩阵后，RandomNodeEdgeSampler针对无向图的特化

--- src/engine/edge_sampling/RandomNodeEdgeSampler.cpp
+++ src/engine/edge_sampling/RandomNodeEdgeSampler.cpp
@@ -9,7 +9,7 @@ using namespace std;
 RandomNodeEdgeSampler::RandomNodeEdgeSampler(size_t number_of_edges, int seed)
     : EdgeSampler(number_of_edges, seed) {}
 
-vector<pair<size_t, size_t>> RandomNodeEdgeSampler::_sample(const Graph& g) {
+vector<pair<size_t, size_t>> RandomNodeEdgeSampler::sample(const DirectedGraph& g) {
     auto ret = vector<pair<size_t, size_t>>();
 
     const auto& columns = g.get_columns();
@@ -30,10 +30,29 @@ vector<pair<size_t, size_t>> RandomNodeEdgeSampler::_sample(const Graph& g) {
     return ret;
 }
 
-vector<pair<size_t, size_t>> RandomNodeEdgeSampler::sample(const DirectedGraph& g) {
-    return _sample(g);
-}
-
 vector<pair<size_t, size_t>> RandomNodeEdgeSampler::sample(const UndirectedGraph& g) {
-    return _sample(g);
+    auto ret = vector<pair<size_t, size_t>>();
+
+    const auto& columns = g.get_columns();
+    const auto& offsets = g.get_offsets();
+    const auto& degrees = g.get_degrees();
+
+    auto   edge_is_sampled = vector<bool>(g.number_of_edges());
+    size_t current_sampled = 0;
+    while (current_sampled < number_of_edges) {
+        auto i = random.randint<size_t>(g.number_of_nodes());
+        if (!degrees[i]) continue;
+        auto loc = offsets[i] + random.randint<size_t>(degrees[i]);
+        if (edge_is_sampled[loc]) continue;
+        auto j                                       = columns[loc];
+        edge_is_sampled[loc]                         = true;
+        edge_is_sampled[g.loc_of_edge_between(j, i)] = true;
+        if (i < j) {
+            ret.emplace_back(i, j);
+        } else {
+            ret.emplace_back(j, i);
+        }
+        ++current_sampled;
+    }
+    return ret;
 }
