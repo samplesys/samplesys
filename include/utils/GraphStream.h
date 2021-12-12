626537abb46ac51cef8a7c1eef171a7b522f91bb
Autumnal_Joy <autumnal_joy@foxmail.com>
2021-12-12 23:39:34 +0800

    feature: 增加文本文件转二进制文件，加快转换速度

--- include/utils/GraphStream.h
+++ include/utils/GraphStream.h
@@ -17,6 +17,9 @@ class GraphStream {
 
     static std::shared_ptr<Graph> readBinary(const std::string &filename, bool directed = true);
     static void writeBinary(const std::string &filename, const std::shared_ptr<Graph> &g);
+
+    static void text_to_binary(const std::string &input_filename,
+                               const std::string &output_filename, bool directed = true);
 };
 
 #endif  // SAMPLER_SRC_UTILS_GRAPHSTREAM_H_
