e40acfed7ca9ef756ccfe0e1052252d6c61b4adf
Autumnal_Joy <autumnal_joy@foxmail.com>
2022-05-15 11:17:02 +0800

    reformat

--- src/analysis/linear.cpp
+++ src/analysis/linear.cpp
@@ -4,7 +4,8 @@ void get_eigen(arma::vec &eigval, arma::mat &eigvec, const arma::sp_mat &adj_mat
     eigs_sym(eigval, eigvec, adj_mat, k, "la");
 }
 
-void get_singular(arma::mat &U, arma::vec &S, arma::mat &V, const arma::sp_mat &adj_mat, const int &k) {
+void get_singular(arma::mat &U, arma::vec &S, arma::mat &V, const arma::sp_mat &adj_mat,
+                  const int &k) {
     svds(U, S, V, adj_mat, k);
 }
 }  // namespace Backend
