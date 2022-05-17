ebbdd91586edf0c2853ff334b8d48bb1033321dc
Autumnal_Joy <autumnal_joy@foxmail.com>
2022-05-17 22:02:42 +0800

    feature: 增加RandomWalkSampler

---
+++ include/engine/exploration_sampling/RandomWalkSampler.h
@@ -0,0 +1,24 @@
+/*
+ * Created by AutJ on 2022-05-17.
+ */
+
+#ifndef SAMPLER_SRC_ENGINE_EXPLORATION_SAMPLING_RANDOMWALKSAMPLER_H_
+#define SAMPLER_SRC_ENGINE_EXPLORATION_SAMPLING_RANDOMWALKSAMPLER_H_
+
+#include "engine/BaseSampler.h"
+
+class RandomWalkSampler : public BaseSampler {
+   private:
+    std::size_t number_of_nodes;
+    std::size_t start_node;
+
+    std::vector<std::pair<std::size_t, std::size_t>> _sample(const DirectedGraph *g);
+    std::vector<std::pair<std::size_t, std::size_t>> _sample(const UndirectedGraph *g);
+
+   public:
+    explicit RandomWalkSampler(std::size_t number_of_nodes, std::size_t start_node = -1,
+                               int seed = 10);
+    std::vector<std::pair<std::size_t, std::size_t>> sample(const Graph &g) override;
+};
+
+#endif  // SAMPLER_SRC_ENGINE_EXPLORATION_SAMPLING_RANDOMWALKSAMPLER_H_
---
+++ src/engine/exploration_sampling/RandomWalkSampler.cpp
@@ -0,0 +1,97 @@
+/*
+ * Created by AutJ on 2022-05-17.
+ */
+
+#include "engine/exploration_sampling/RandomWalkSampler.h"
+
+using namespace std;
+
+RandomWalkSampler::RandomWalkSampler(size_t number_of_nodes, size_t start_node, int seed)
+    : BaseSampler(seed), number_of_nodes(number_of_nodes), start_node(start_node) {}
+
+vector<pair<size_t, size_t>> RandomWalkSampler::_sample(const DirectedGraph* g) {
+    auto ret = vector<pair<size_t, size_t>>();
+
+    const auto& columns = g->get_columns();
+    const auto& offsets = g->get_offsets();
+    const auto& degrees = g->get_degrees();
+
+    size_t source;
+    if (start_node > g->number_of_nodes()) {
+        source = random.randint(g->number_of_nodes());
+    } else {
+        source = start_node;
+    }
+
+    auto   node_is_sampled       = vector<bool>(g->number_of_nodes());
+    size_t current_sampled_nodes = 0;
+
+    while (current_sampled_nodes < number_of_nodes) {
+        if (!node_is_sampled[source]) {
+            node_is_sampled[source] = true;
+            ++current_sampled_nodes;
+        }
+        
+        source = columns[offsets[source] + random.randint(degrees[source])];
+    }
+
+    for (size_t i = 0; i < g->number_of_nodes(); ++i) {
+        if (!node_is_sampled[i]) continue;
+        for (auto loc = offsets[i]; loc < offsets[i + 1]; ++loc) {
+            if (node_is_sampled[columns[loc]]) {
+                ret.emplace_back(i, columns[loc]);
+            }
+        }
+    }
+
+    return ret;
+}
+
+vector<pair<size_t, size_t>> RandomWalkSampler::_sample(const UndirectedGraph* g) {
+    auto ret = vector<pair<size_t, size_t>>();
+
+    const auto& columns = g->get_columns();
+    const auto& offsets = g->get_offsets();
+    const auto& degrees = g->get_degrees();
+
+    size_t source;
+    if (start_node > g->number_of_nodes()) {
+        source = random.randint(g->number_of_nodes());
+    } else {
+        source = start_node;
+    }
+
+    auto   node_is_sampled       = vector<bool>(g->number_of_nodes());
+    size_t current_sampled_nodes = 0;
+
+    while (current_sampled_nodes < number_of_nodes) {
+        if (!node_is_sampled[source]) {
+            node_is_sampled[source] = true;
+            ++current_sampled_nodes;
+        }
+        source = columns[offsets[source] + random.randint(degrees[source])];
+    }
+
+    for (size_t i = 0; i < g->number_of_nodes(); ++i) {
+        if (!node_is_sampled[i]) continue;
+        for (auto loc = offsets[i]; loc < offsets[i + 1]; ++loc) {
+            if (i < offsets[loc] && node_is_sampled[columns[loc]]) {
+                ret.emplace_back(i, columns[loc]);
+            }
+        }
+    }
+
+    return ret;
+}
+
+vector<pair<size_t, size_t>> RandomWalkSampler::sample(const Graph& g) {
+    auto ptr1 = dynamic_cast<const DirectedGraph*>(&g);
+    if (ptr1 != nullptr) {
+        return this->_sample(ptr1);
+    }
+    auto ptr2 = dynamic_cast<const UndirectedGraph*>(&g);
+    if (ptr2 != nullptr) {
+        return this->_sample(ptr2);
+    }
+    return {};
+}
